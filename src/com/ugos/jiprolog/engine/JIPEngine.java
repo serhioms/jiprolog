d6f591eb41e9066cc528e9cb7ff8e2f4a7e95d33
serhioms <sergey.moskovskiy@gmail.com>
2020-06-29 18:05:13 -0400

    jiprolog binaries

--- src/com/ugos/jiprolog/engine/JIPEngine.java
+++ src/com/ugos/jiprolog/engine/JIPEngine.java
@@ -318,7 +318,7 @@ public class JIPEngine implements Serializable
             if(entry.getName().endsWith(".pl"))
             {
                 String strCurSarchPath = "";
-                System.out.println("Load "+entry);
+//                System.out.println("Load "+entry);
                 final InputStream ins = library.getInputStream(entry);
                 try
                 {
